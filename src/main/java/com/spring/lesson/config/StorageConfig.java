c075c77cbc7c7df35cbf5e0abaf8c25f0a96c556
Oleksandr Yakovenko <yakovenko9814@ukr.net>
2020-01-10 17:55:42 +0200

    05 Refactor Application.class

---
+++ src/main/java/com/spring/lesson/config/StorageConfig.java
@@ -0,0 +1,21 @@
+package com.spring.lesson.config;
+
+import com.spring.lesson.properties.StorageProperties;
+import com.spring.lesson.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(StorageProperties.class)
+public class StorageConfig {
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+}
